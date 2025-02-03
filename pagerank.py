d177a4284d1343e837e6ecca8a32a4d5dc2d98f6
teocario99 <166152775+teocario99@users.noreply.github.com>
2025-02-03 16:46:40 +0100

    add pagerank func

--- pagerank.py
+++ pagerank.py
@@ -13,3 +13,19 @@ pos = nx.shell_layout(myWeb)
 nx.draw(myWeb, pos, arrows=True, with_labels=True)
 plt.show()
 
+def createPageRank(aGraph):
+    nodes_set = len(aGraph)
+    M = nx.to_numpy_array(aGraph)
+    outwards = np.squeeze(np.asarray(np.sum(M, axis=1)))
+    prob_outwards = np.array(
+        [1.0/count
+         if count > 0 else 0.0 for count in outwards])
+    G = np.asarray(np.multiply(M.T, prob_outwards))
+    p = np.ones(nodes_set) / float(nodes_set)
+    if np.min(np.sum(G, axis=0)) < 1.0:
+        print('WARN: G is substochastic')
+    return G, p
+
+G, p = createPageRank(myWeb)
+print(G)
+
